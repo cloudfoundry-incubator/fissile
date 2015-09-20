702134c85f823f408374336ee20dde700e9f54d3
Vlad Iovanov <viovanov@hotmail.com>
2015-09-20 00:01:04 -0700

    Implement compilator folder structure setup and add tests

--- compilator/compilator.go
+++ compilator/compilator.go
@@ -1,75 +1,197 @@
 package compilator
 
 import (
-	)
-	
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/hpcloud/fissile/docker"
+	"github.com/hpcloud/fissile/model"
+
+	"github.com/crufter/copyrecur"
+)
+
 const (
 	ContainerPackagesDir = "/var/vcap/packages"
-	)
-	
+)
+
 const (
-	packageNone = iota
+	packageError = iota
+	packageNone
 	packageCompiling
 	packageCompiled
 )
-	
+
 type Compilator struct {
 	DockerManager *docker.DockerImageManager
-	Release *model.Release
-	HostWorkDir string
-	
-	packageLock []sync.Mutex
-	packageCompiling hash[string]false
+	Release       *model.Release
+	HostWorkDir   string
+
+	packageLock      map[*model.Package]*sync.Mutex
+	packageCompiling map[*model.Package]bool
 }
 
-
 func NewCompilator(dockerManager *docker.DockerImageManager, release *model.Release, hostWorkDir string) (*Compilator, error) {
-	compilator := &Compilator {
+	compilator := &Compilator{
 		DockerManager: dockerManager,
-		Release: release,
-		HostWorkDir: hostWorkDir,
+		Release:       release,
+		HostWorkDir:   hostWorkDir,
+
+		packageLock:      map[*model.Package]*sync.Mutex{},
+		packageCompiling: map[*model.Package]bool{},
 	}
-	
+
+	for _, pkg := range release.Packages {
+		compilator.packageLock[pkg] = &sync.Mutex{}
+		compilator.packageCompiling[pkg] = false
+	}
+
 	return compilator, nil
 }
 
 func (c *Compilator) Compile(workerCount int) error {
 	// Check for cycles
-	
+
 	// Iterate until all packages are compiled
-	// Not the most efficient implementation, 
+	// Not the most efficient implementation,
 	// but it's easy to parallelize and reason about
 	for workerIdx := 0; workerIdx < workerCount; workerIdx++ {
-		go func (idx int) {
+		go func(idx int) {
 			// Wait a bit if there's nothing to work on
 		}(workerIdx)
 	}
-	
+
 	// Wait until all workers finish
+
+	return nil
 }
 
-func (c *Compilator) compilePackage(model.Package pkg) error {
+func (c *Compilator) compilePackage(pkg *model.Package) error {
 	// Do nothing if any dependency has not been compiled
 	for _, dep := range pkg.Dependencies {
-		if getPackageStatus(dep) != packageCompiled {
+
+		packageStatus, err := c.getPackageStatus(dep)
+		if err != nil {
+			return err
+		}
+
+		if packageStatus != packageCompiled {
 			return nil
 		}
 	}
-	
+
 	// Compile
 	// Prepare input dir (package plus deps), generate a compilation script, run compilation in container,
 	// remove container, copy compiled package to its destination in the work dir
+
+	if err := c.createCompilationDirStructure(pkg); err != nil {
+		return err
+	}
+
+	if err := c.copyDependencies(pkg); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (c *Compilator) getPackageStatus(mode.Package pkg) (int, error) {
+// We want to create a package structure like this:
+// .
+// └── <pkg-name>
+//     ├── compiled
+//     └── sources
+//         └── var
+//             └── vcap
+//                 ├── packages
+//                 │   └── <dependency-package>
+//                 └── source
+func (c *Compilator) createCompilationDirStructure(pkg *model.Package) error {
+	dependenciesPackageDir := c.getDependenciesPackageDir(pkg)
+	sourcePackageDir := c.getSourcePackageDir(pkg)
+
+	if err := os.MkdirAll(dependenciesPackageDir, 0755); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(sourcePackageDir, 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Compilator) getTargetPackageSourcesDir(pkg *model.Package) string {
+	return filepath.Join(c.HostWorkDir, pkg.Name, "sources")
+}
+
+func (c *Compilator) getPackageCompiledDir(pkg *model.Package) string {
+	return filepath.Join(c.HostWorkDir, pkg.Name, "compiled")
+}
+
+func (c *Compilator) getDependenciesPackageDir(pkg *model.Package) string {
+	return filepath.Join(c.getTargetPackageSourcesDir(pkg), "var", "vcap", "packages")
+}
+
+func (c *Compilator) getSourcePackageDir(pkg *model.Package) string {
+	return filepath.Join(c.getTargetPackageSourcesDir(pkg), "var", "vcap", "source")
+}
+
+func (c *Compilator) copyDependencies(pkg *model.Package) error {
+	for _, dep := range pkg.Dependencies {
+		depCompiledPath := c.getPackageCompiledDir(dep)
+		depDestinationPath := filepath.Join(c.getDependenciesPackageDir(pkg), dep.Name)
+		if err := copyrecur.CopyDir(depCompiledPath, depDestinationPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *Compilator) getPackageStatus(pkg *model.Package) (int, error) {
 	// Acquire mutex before checking status
-	
+	c.packageLock[pkg].Lock()
+	defer func() {
+		c.packageLock[pkg].Unlock()
+	}()
+
 	// If package is in packageCompiling hash
-	return packageCompiling, nil
-	
+	if c.packageCompiling[pkg] {
+		return packageCompiling, nil
+	}
+
 	// If compiled package exists on hard
-	return packageCompiled, nil
-	
+	compiledPackagePath := filepath.Join(c.HostWorkDir, pkg.Name, "compiled")
+	compiledPackageExists, err := validatePath(compiledPackagePath, true, "package path")
+	if err != nil {
+		return packageError, err
+	}
+
+	if compiledPackageExists {
+		return packageCompiled, nil
+	}
+
 	// Package is in no state otherwise
 	return packageNone, nil
+}
+
+func validatePath(path string, shouldBeDir bool, pathDescription string) (bool, error) {
+	pathInfo, err := os.Stat(path)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+
+	if pathInfo.IsDir() && !shouldBeDir {
+		return false, fmt.Errorf("Path %s (%s) points to a directory. It should be a a file. %s", path, pathDescription)
+	} else if !pathInfo.IsDir() && shouldBeDir {
+		return false, fmt.Errorf("Path %s (%s) points to a file. It should be a directory.", path, pathDescription)
+	}
+
+	return true, nil
 }
---
+++ compilator/compilator_test.go
@@ -0,0 +1,161 @@
+package compilator
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/hpcloud/fissile/docker"
+	"github.com/hpcloud/fissile/model"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	dockerEndpointEnvVar      = "FISSILE_TEST_DOCKER_ENDPOINT"
+	defaultDockerTestEndpoint = "unix:///var/run/docker.sock"
+	dockerImageEnvVar         = "FISSILE_TEST_DOCKER_IMAGE"
+	defaultDockerTestImage    = "ubuntu:14.04"
+)
+
+var dockerEndpoint string
+var dockerImageName string
+
+func TestMain(m *testing.M) {
+	dockerEndpoint = os.Getenv(dockerEndpointEnvVar)
+	if dockerEndpoint == "" {
+		dockerEndpoint = defaultDockerTestEndpoint
+	}
+
+	dockerImageName = os.Getenv(dockerImageEnvVar)
+	if dockerImageName == "" {
+		dockerImageName = defaultDockerTestImage
+	}
+
+	retCode := m.Run()
+
+	os.Exit(retCode)
+}
+
+func TestCompilation(t *testing.T) {
+}
+
+func TestCompilationSourcePreparation(t *testing.T) {
+}
+
+func TestGetPackageStatusCompiled(t *testing.T) {
+	assert := assert.New(t)
+
+	compilationWorkDir, err := ioutil.TempDir("", "fissile-tests")
+	assert.Nil(err)
+
+	dockerManager, err := docker.NewDockerImageManager(dockerEndpoint)
+	assert.Nil(err)
+
+	workDir, err := os.Getwd()
+	ntpReleasePath := filepath.Join(workDir, "../test-assets/ntp-release-2")
+	release, err := model.NewRelease(ntpReleasePath)
+	assert.Nil(err)
+
+	compilator, err := NewCompilator(dockerManager, release, compilationWorkDir)
+	assert.Nil(err)
+
+	compiledPackagePath := filepath.Join(compilationWorkDir, release.Packages[0].Name, "compiled")
+	err = os.MkdirAll(compiledPackagePath, 0755)
+	assert.Nil(err)
+	status, err := compilator.getPackageStatus(release.Packages[0])
+
+	assert.Nil(err)
+	assert.Equal(packageCompiled, status)
+}
+
+func TestGetPackageStatusNone(t *testing.T) {
+	assert := assert.New(t)
+
+	compilationWorkDir, err := ioutil.TempDir("", "fissile-tests")
+	assert.Nil(err)
+
+	dockerManager, err := docker.NewDockerImageManager(dockerEndpoint)
+	assert.Nil(err)
+
+	workDir, err := os.Getwd()
+	ntpReleasePath := filepath.Join(workDir, "../test-assets/ntp-release-2")
+	release, err := model.NewRelease(ntpReleasePath)
+	assert.Nil(err)
+
+	compilator, err := NewCompilator(dockerManager, release, compilationWorkDir)
+	assert.Nil(err)
+
+	status, err := compilator.getPackageStatus(release.Packages[0])
+
+	assert.Nil(err)
+	assert.Equal(packageNone, status)
+}
+
+func TestPackageFolderStructure(t *testing.T) {
+	assert := assert.New(t)
+
+	compilationWorkDir, err := ioutil.TempDir("", "fissile-tests")
+	assert.Nil(err)
+
+	dockerManager, err := docker.NewDockerImageManager(dockerEndpoint)
+	assert.Nil(err)
+
+	workDir, err := os.Getwd()
+	ntpReleasePath := filepath.Join(workDir, "../test-assets/ntp-release-2")
+	release, err := model.NewRelease(ntpReleasePath)
+	assert.Nil(err)
+
+	compilator, err := NewCompilator(dockerManager, release, compilationWorkDir)
+	assert.Nil(err)
+
+	err = compilator.createCompilationDirStructure(release.Packages[0])
+	assert.Nil(err)
+
+	exists, err := validatePath(compilator.getDependenciesPackageDir(release.Packages[0]), true, "")
+	assert.Nil(err)
+	assert.True(exists)
+
+	exists, err = validatePath(compilator.getSourcePackageDir(release.Packages[0]), true, "")
+	assert.Nil(err)
+	assert.True(exists)
+}
+
+func TestPackageDependenciesPreparation(t *testing.T) {
+	assert := assert.New(t)
+
+	compilationWorkDir, err := ioutil.TempDir("", "fissile-tests")
+	assert.Nil(err)
+
+	dockerManager, err := docker.NewDockerImageManager(dockerEndpoint)
+	assert.Nil(err)
+
+	workDir, err := os.Getwd()
+	ntpReleasePath := filepath.Join(workDir, "../test-assets/tor-boshrelease-0.3.5")
+	release, err := model.NewRelease(ntpReleasePath)
+	assert.Nil(err)
+
+	compilator, err := NewCompilator(dockerManager, release, compilationWorkDir)
+	assert.Nil(err)
+
+	pkg, err := compilator.Release.LookupPackage("tor")
+	assert.Nil(err)
+	err = compilator.createCompilationDirStructure(pkg)
+	assert.Nil(err)
+	err = os.MkdirAll(compilator.getPackageCompiledDir(pkg.Dependencies[0]), 0755)
+	assert.Nil(err)
+
+	dummyCompiledFile := filepath.Join(compilator.getPackageCompiledDir(pkg.Dependencies[0]), "foo")
+	file, err := os.Create(dummyCompiledFile)
+	assert.Nil(err)
+	file.Close()
+
+	err = compilator.copyDependencies(pkg)
+	assert.Nil(err)
+
+	expectedDummyFileLocation := filepath.Join(compilator.getDependenciesPackageDir(pkg), pkg.Dependencies[0].Name, "foo")
+	exists, err := validatePath(expectedDummyFileLocation, false, "")
+	assert.Nil(err)
+	assert.True(exists, expectedDummyFileLocation)
+}
