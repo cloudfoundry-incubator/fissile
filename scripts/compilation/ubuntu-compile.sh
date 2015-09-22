89c4e1766e1556830b887ecbf40c832242edb6ad
vladi <vlad.iovanov@hpe.com>
2015-09-22 03:29:12 +0000

    Fix ubuntu package compilation script

--- scripts/compilation/ubuntu-compile.sh
+++ scripts/compilation/ubuntu-compile.sh
@@ -3,25 +3,31 @@ set -e # exit immediately if a simple command exits with a non-zero status
 packageName=$1
 packageVersion=$2
 
-if [ -z "$packageName" ]; then
-  echo "Package name not specified" >&2
+if [ -z "$packageName" ];
+then
+  echo "Package name not specified" 1>&2
+  exit 1
 fi
 
-if [ -z "$packageVersion" ]; then
-  echo "Package version not specified" >&2
+if [ -z "$packageVersion" ];
+then
+  echo "Package version not specified" 1>&2
+  exit 1
 fi
-
-
-if [ -z "$VAR" ];
 
 mkdir -p /var/vcap
 
 cp -r /fissile-in/var/vcap/* /var/vcap
 
-export BOSH_COMPILE_TARGET=/var/vcap/source
-export BOSH_INSTALL_TARGET=/fissile-out
+export BOSH_COMPILE_TARGET="/var/vcap/source/$packageName"
+export BOSH_INSTALL_TARGET="/var/vcap/packages/$packageName"
 export BOSH_PACKAGE_NAME=$packageName
 export BOSH_PACKAGE_VERSION=$packageVersion
 
+echo "Compiling to $BOSH_INSTALL_TARGET"
+
+ln -s /fissile-out $BOSH_INSTALL_TARGET
+
 cd $BOSH_COMPILE_TARGET
-bash -x ./packaging
+bash ./packaging
+
