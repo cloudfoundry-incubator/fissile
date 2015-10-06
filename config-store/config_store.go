84a29c054e25d672a6baf809e9b2e51cbe678e35
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2015-10-06 13:19:35 -0700

    Create a configuration base file that can be imported into consul

--- config-store/config_store.go
+++ config-store/config_store.go
@@ -2,91 +2,39 @@ package config_store
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
-
-	"github.com/hashicorp/consul/api"
 )
 
 type ConfigStoreManager struct {
-	client *api.Client
 	prefix string
 }
 
-func NewConfigStoreManager(prefix, consulURL string) (*ConfigStoreManager, error) {
-
-	parsedURL, err := url.Parse(consulURL)
-	if err != nil {
-		return nil, err
-	}
-
-	consulScheme := parsedURL.Scheme
-	consulAddress := parsedURL.Host
-	consulDatacenter := strings.Trim(parsedURL.Path, "/")
-	consulUsername := ""
-	consulPassword := ""
-	if parsedURL.User != nil {
-		consulUsername = parsedURL.User.Username()
-		consulPassword, _ = parsedURL.User.Password()
-	}
-
-	clientConfig := &api.Config{
-		Address:    consulAddress,
-		Scheme:     consulScheme,
-		Datacenter: consulDatacenter,
-		HttpAuth: &api.HttpBasicAuth{
-			Username: consulUsername,
-			Password: consulPassword,
-		},
-	}
-
-	consulClient, err := api.NewClient(clientConfig)
-	if err != nil {
-		return nil, err
-	}
-
+func NewConfigStoreManager(prefix string) *ConfigStoreManager {
 	configStoreManager := &ConfigStoreManager{
-		client: consulClient,
 		prefix: prefix,
 	}
 
-	return configStoreManager, nil
-}
-
-func (c *ConfigStoreManager) AddBOSHKey(key, description string, value interface{}) error {
-	return nil
-}
-
-func (c *ConfigStoreManager) DeleteBOSHKey(key string) error {
-	return nil
-}
-
-func (c *ConfigStoreManager) ListBOSHKeys() ([]string, error) {
-	return nil, nil
-}
-
-func (c *ConfigStoreManager) GetBOSHKey(key string) (interface{}, error) {
-	return nil, nil
+	return configStoreManager
 }
 
 func (c *ConfigStoreManager) boshKeyToConsulPath(key string) (string, error) {
 	keyGrams := strings.FieldsFunc(key, func(c rune) bool { return c == '.' })
 	if len(keyGrams) == 0 {
-		return "", fmt.Errorf("BOSH config key cannot be empty", key)
+		return "", fmt.Errorf("BOSH config key cannot be empty")
 	}
 
 	keyGrams = append([]string{"", c.prefix}, keyGrams...)
-	return strings.Join(keyGrams, "/")
+	return strings.Join(keyGrams, "/"), nil
 }
 
 func (c *ConfigStoreManager) consulPathToBOSHKey(key string) (string, error) {
 	keyGrams := strings.FieldsFunc(key, func(c rune) bool { return c == '/' })
-	if len(keyGrams) < 1 {
-		return fmt.Errorf("Configuration key %s is not a valid consul key", key)
+	if len(keyGrams) < 2 {
+		return "", fmt.Errorf("Configuration key %s is not a valid consul key", key)
 	}
 
 	if keyGrams[0] != c.prefix {
-		return fmt.Errorf("Consul configuration key %s does not have the correct prefix %s", key, c.prefix)
+		return "", fmt.Errorf("Consul configuration key %s does not have the correct prefix %s", key, c.prefix)
 	}
 
 	return strings.Join(keyGrams[1:], "."), nil
--- config-store/config_store_test.go
+++ config-store/config_store_test.go
@@ -9,13 +9,66 @@ import (
 func TestBOSHKeyToConsulPathConversion(t *testing.T) {
 	assert := assert.New(t)
 
-	confStore, err := NewConfigStoreManager("foo", "http://foo:5800/dc1")
-	assert.Nil(err)
+	confStore := NewConfigStoreManager("foo")
 
 	boshKey := "this.is.a.bosh.key"
 
-	consulPath := confStore.boshKeyToConsulPath(boshKey)
+	consulPath, err := confStore.boshKeyToConsulPath(boshKey)
+
+	assert.Nil(err)
 
 	assert.Equal("/foo/this/is/a/bosh/key", consulPath)
 
+}
+
+func TestBOSHKeyToConsulPathConversionError(t *testing.T) {
+	assert := assert.New(t)
+
+	confStore := NewConfigStoreManager("foo")
+
+	boshKey := ""
+
+	_, err := confStore.boshKeyToConsulPath(boshKey)
+
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "BOSH config key cannot be empty")
+}
+
+func TestConsulPathToBOSHKeyConversion(t *testing.T) {
+	assert := assert.New(t)
+
+	confStore := NewConfigStoreManager("foo")
+
+	boshKey := "/foo/this/is/a/consul/path"
+
+	boshKey, err := confStore.consulPathToBOSHKey(boshKey)
+
+	assert.Nil(err)
+	assert.Equal("this.is.a.consul.path", boshKey)
+}
+
+func TestConsulPathToBOSHKeyConversionBadPrefix(t *testing.T) {
+	assert := assert.New(t)
+
+	confStore := NewConfigStoreManager("foo")
+
+	boshKey := "/bar/this/is/a/consul/path"
+
+	boshKey, err := confStore.consulPathToBOSHKey(boshKey)
+
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "does not have the correct prefix")
+}
+
+func TestConsulPathToBOSHKeyConversionBadKey(t *testing.T) {
+	assert := assert.New(t)
+
+	confStore := NewConfigStoreManager("foo")
+
+	boshKey := "/foo/"
+
+	boshKey, err := confStore.consulPathToBOSHKey(boshKey)
+
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "is not a valid consul key")
 }
