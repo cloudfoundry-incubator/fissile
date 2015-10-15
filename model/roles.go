39780b5c5c0369315a858581ca4d5e3afedce30f
Vlad Iovanov <vlad.iovanov@hp.com>
2015-10-15 16:20:44 -0700

    Add role scripts to role manifest

--- model/roles.go
+++ model/roles.go
@@ -3,19 +3,24 @@ package model
 import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"path/filepath"
 )
 
 // RoleManifest represents a collection of roles
 type RoleManifest struct {
 	Roles []*Role `yaml:"roles"`
+
+	manifestFilePath string
 }
 
 // Role represents a collection of jobs that are colocated on a container
 type Role struct {
-	Name string `yaml:"name"`
-	Jobs []*Job `yaml:"_,omitempty"`
-
+	Name        string     `yaml:"name"`
+	Jobs        []*Job     `yaml:"_,omitempty"`
+	Scripts     []string   `yaml:"scripts"`
 	JobNameList []*roleJob `yaml:"jobs"`
+
+	rolesManifest *RoleManifest
 }
 
 type roleJob struct {
@@ -30,11 +35,13 @@ func LoadRoleManifest(manifestFilePath string, release *Release) (*RoleManifest,
 	}
 
 	rolesManifest := RoleManifest{}
+	rolesManifest.manifestFilePath = manifestFilePath
 	if err := yaml.Unmarshal(manifestContents, &rolesManifest); err != nil {
 		return nil, err
 	}
 
 	for _, role := range rolesManifest.Roles {
+		role.rolesManifest = &rolesManifest
 		role.Jobs = make([]*Job, len(role.JobNameList))
 
 		for idx, roleJob := range role.JobNameList {
@@ -48,4 +55,18 @@ func LoadRoleManifest(manifestFilePath string, release *Release) (*RoleManifest,
 	}
 
 	return &rolesManifest, nil
+}
+
+func (r *Role) GetScriptPaths() []string {
+	if r.Scripts == nil {
+		return []string{}
+	}
+
+	result := make([]string, len(r.Scripts))
+	for idx := range r.Scripts {
+		result[idx] = filepath.Join(filepath.Dir(r.rolesManifest.manifestFilePath), r.Scripts[idx])
+	}
+
+	return result
+
 }
