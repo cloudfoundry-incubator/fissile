bf5772d544e33e35951cdd7ec9ed9aebe0330489
Eric Promislow <eric.promislow@hpe.com>
2016-11-30 11:19:00 -0800

    HCF-1124 return quickly

--- app/fissile.go
+++ app/fissile.go
@@ -42,21 +42,22 @@ func NewFissileApplication(version string, ui *termui.UI) *Fissile {
 
 // SetPatchPropertiesDirective saves the patch-properties release and job names, if specified.
 func (f *Fissile) SetPatchPropertiesDirective(patchPropertiesDirective string) error {
-	if patchPropertiesDirective != "" {
-		msgStart := "Invalid format for --patch-properties-release flag: should be RELEASE/JOB;"
-		parts := strings.SplitN(patchPropertiesDirective, "/", 2)
-		if len(parts) <= 1 {
-			return fmt.Errorf(msgStart + " only one part is specified")
-		}
-		if len(parts[0]) == 0 {
-			return fmt.Errorf(msgStart + " no RELEASE is specified")
-		}
-		if len(parts[1]) == 0 {
-			return fmt.Errorf(msgStart + " no JOB is specified")
-		}
-		f.patchPropertiesReleaseName = parts[0]
-		f.patchPropertiesJobName = parts[1]
+	if patchPropertiesDirective == "" {
+		return nil
 	}
+	msgStart := "Invalid format for --patch-properties-release flag: should be RELEASE/JOB;"
+	parts := strings.SplitN(patchPropertiesDirective, "/", 2)
+	if len(parts) <= 1 {
+		return fmt.Errorf(msgStart + " only one part is specified")
+	}
+	if len(parts[0]) == 0 {
+		return fmt.Errorf(msgStart + " no RELEASE is specified")
+	}
+	if len(parts[1]) == 0 {
+		return fmt.Errorf(msgStart + " no JOB is specified")
+	}
+	f.patchPropertiesReleaseName = parts[0]
+	f.patchPropertiesJobName = parts[1]
 	return nil
 }
 
