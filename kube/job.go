17a4e663adc82fb066e98827125107c31a0e00de
Mark Yen <mook@hpe.com>
2017-02-10 11:19:10 -0800

    kube: better guarding when creating jobs

--- kube/job.go
+++ kube/job.go
@@ -1,6 +1,8 @@
 package kube
 
 import (
+	"fmt"
+
 	"github.com/hpcloud/fissile/model"
 	meta "k8s.io/client-go/pkg/api/unversioned"
 	"k8s.io/client-go/pkg/api/v1"
@@ -13,11 +15,19 @@ func NewJob(role *model.Role, settings *ExportSettings) (*extra.Job, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	if role.Run == nil {
+		return nil, fmt.Errorf("Role %s has no run information", role.Name)
+	}
+
 	// Jobs must have a restart policy that isn't "always"
-	if role.Run.FlightStage == model.FlightStageManual {
+	switch role.Run.FlightStage {
+	case model.FlightStageManual:
 		podTemplate.Spec.RestartPolicy = v1.RestartPolicyNever
-	} else {
+	case model.FlightStageFlight, model.FlightStagePreFlight, model.FlightStagePostFlight:
 		podTemplate.Spec.RestartPolicy = v1.RestartPolicyOnFailure
+	default:
+		return nil, fmt.Errorf("Role %s has unexpected flight stage %s", role.Name, role.Run.FlightStage)
 	}
 
 	return &extra.Job{
