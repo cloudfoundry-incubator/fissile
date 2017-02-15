2e551f943182c711add669476be6df7ac7ba5f86
Mark Yen <mook@hpe.com>
2017-02-15 11:08:30 -0800

    kube: add custom readiness probes
    This adds support for URL and custom commands in readiness probes

--- kube/pod_test.go
+++ kube/pod_test.go
@@ -1,6 +1,7 @@
 package kube
 
 import (
+	"encoding/base64"
 	"os"
 	"path/filepath"
 	"testing"
@@ -9,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/pkg/api/resource"
 	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/util/intstr"
 )
 
 func podTestLoadRole(assert *assert.Assertions) *model.Role {
@@ -240,6 +242,194 @@ func TestPodGetContainerPorts(t *testing.T) {
 		if sample.err != "" {
 			assert.EqualError(err, sample.err, sample.desc)
 		} else {
+			assert.Equal(sample.expected, actual, sample.desc)
+		}
+	}
+}
+
+func TestPodGetContainerReadinessProbe(t *testing.T) {
+	assert := assert.New(t)
+	role := podTestLoadRole(assert)
+	if role == nil {
+		return
+	}
+
+	samples := []struct {
+		desc     string
+		probe    *model.ReadinessProbe
+		expected *v1.Probe
+		err      string
+	}{
+		{
+			desc:     "No probe",
+			probe:    nil,
+			expected: nil,
+		},
+		{
+			desc: "Port probe",
+			probe: &model.ReadinessProbe{
+				Port: 1234,
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					TCPSocket: &v1.TCPSocketAction{
+						Port: intstr.FromInt(1234),
+					},
+				},
+			},
+		},
+		{
+			desc: "Command probe",
+			probe: &model.ReadinessProbe{
+				Command: []string{"rm", "-rf", "--no-preserve-root", "/"},
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					Exec: &v1.ExecAction{
+						Command: []string{"rm", "-rf", "--no-preserve-root", "/"},
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (simple)",
+			probe: &model.ReadinessProbe{
+				URL: "http://example.com/path",
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTP,
+						Host:   "example.com",
+						Port:   intstr.FromInt(80),
+						Path:   "/path",
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (custom port)",
+			probe: &model.ReadinessProbe{
+				URL: "https://example.com:1234/path",
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTPS,
+						Host:   "example.com",
+						Port:   intstr.FromInt(1234),
+						Path:   "/path",
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (Invalid scheme)",
+			probe: &model.ReadinessProbe{
+				URL: "file:///etc/shadow",
+			},
+			err: "Readiness probe for myrole has unsupported URI scheme \"file\"",
+		},
+		{
+			desc: "URL probe (query)",
+			probe: &model.ReadinessProbe{
+				URL: "http://example.com/path?query#hash",
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTP,
+						Host:   "example.com",
+						Port:   intstr.FromInt(80),
+						Path:   "/path?query",
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (auth)",
+			probe: &model.ReadinessProbe{
+				URL: "http://user:pass@example.com/path",
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTP,
+						Host:   "example.com",
+						Port:   intstr.FromInt(80),
+						Path:   "/path",
+						HTTPHeaders: []v1.HTTPHeader{
+							{
+								Name:  "Authorization",
+								Value: base64.StdEncoding.EncodeToString([]byte("user:pass")),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (custom headers)",
+			probe: &model.ReadinessProbe{
+				URL:     "http://example.com/path",
+				Headers: map[string]string{"x-header": "some value"},
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTP,
+						Host:   "example.com",
+						Port:   intstr.FromInt(80),
+						Path:   "/path",
+						HTTPHeaders: []v1.HTTPHeader{
+							{
+								Name:  "X-Header",
+								Value: "some value",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "URL probe (invalid URL)",
+			probe: &model.ReadinessProbe{
+				URL: "://",
+			},
+			err: "Invalid URL readiness probe for myrole: parse ://: missing protocol scheme",
+		},
+		{
+			desc: "URL probe (invalid port)",
+			probe: &model.ReadinessProbe{
+				URL: "http://example.com:port_number/",
+			},
+			err: "Failed to get URL port for readiness probe for myrole: invalid host \"example.com:port_number\"",
+		},
+		{
+			desc: "URL probe (localhost)",
+			probe: &model.ReadinessProbe{
+				URL: "http://localhost/path",
+			},
+			expected: &v1.Probe{
+				Handler: v1.Handler{
+					HTTPGet: &v1.HTTPGetAction{
+						Scheme: v1.URISchemeHTTP,
+						Port:   intstr.FromInt(80),
+						Path:   "/path",
+					},
+				},
+			},
+		},
+	}
+
+	// TODO use golang 1.7's subtests
+	for _, sample := range samples {
+		role.Run.ReadinessProbe = sample.probe
+		actual, err := getContainerReadinessProbe(role)
+		if sample.err != "" {
+			assert.EqualError(err, sample.err, sample.desc)
+		} else {
+			assert.NoError(err, sample.desc)
 			assert.Equal(sample.expected, actual, sample.desc)
 		}
 	}
--- model/roles.go
+++ model/roles.go
@@ -68,6 +68,7 @@ type RoleRun struct {
 	VirtualCPUs       int                   `yaml:"virtual-cpus"`
 	ExposedPorts      []*RoleRunExposedPort `yaml:"exposed-ports"`
 	FlightStage       FlightStage           `yaml:"flight-stage"`
+	ReadinessProbe    *ReadinessProbe       `yaml:"readiness-probe,omitempty"`
 }
 
 // RoleRunScaling describes how a role should scale out at runtime
@@ -90,6 +91,14 @@ type RoleRunExposedPort struct {
 	External int32  `yaml:"external"`
 	Internal int32  `yaml:"internal"`
 	Public   bool   `yaml:"public"`
+}
+
+// ReadinessProbe describes a non-standard readiness probe
+type ReadinessProbe struct {
+	URL     string            `yaml:"url"`     // URL for a HTTP GET to return 200~399. Cannot be used with other probes.
+	Headers map[string]string `yaml:"headers"` // Custom headers; only used for URL.
+	Command []string          `yaml:"command"` // Custom command. Cannot be used with other probes.
+	Port    int32             `yaml:"port"`    // Port for a TCP probe. Cannot be used with other probes.
 }
 
 // Roles is an array of Role*
@@ -209,6 +218,23 @@ func LoadRoleManifest(manifestFilePath string, releases []*Release) (*RoleManife
 			rolesManifest.Roles = append(rolesManifest.Roles[:i], rolesManifest.Roles[i+1:]...)
 		default:
 			return nil, fmt.Errorf("Role %s has an invalid type %s", role.Name, role.Type)
+		}
+
+		// Ensure that we don't have conflicting readiness probes
+		if role.Run != nil && role.Run.ReadinessProbe != nil {
+			probes := make([]string, 0, 3)
+			if role.Run.ReadinessProbe.URL != "" {
+				probes = append(probes, "url")
+			}
+			if len(role.Run.ReadinessProbe.Command) > 0 {
+				probes = append(probes, "command")
+			}
+			if role.Run.ReadinessProbe.Port != 0 {
+				probes = append(probes, "port")
+			}
+			if len(probes) != 1 {
+				return nil, fmt.Errorf("Readiness probe for role %s should have exactly one of url, command, or port; got %v", role.Name, probes)
+			}
 		}
 	}
 
