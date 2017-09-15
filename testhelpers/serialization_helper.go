a6b426a5b14a4c43b8f58fb19b32346f47939c29
Jan Dubois <jan.dubois@suse.com>
2017-09-14 21:20:14 -0700

    33E5Exov Improve IsYAMLSubset()
    * Don't call .String() on reflect.Value types; it won't actually return the
      actual value string. Use the magic of the fmt package instead.
    
    * Don't complain about missing scalar elements if their expected value is the
      default/zero value for its type.
    
      With this mechanism the expected structure can include a field with the
      default value to make sure it doesn't have any non-default value in the
      actual structure, but won't complain if the field is completely absent
      either.

--- testhelpers/serialization_helper.go
+++ testhelpers/serialization_helper.go
@@ -36,8 +36,20 @@ func isYAMLSubsetInner(assert *assert.Assertions, expected, actual interface{}, 
 			}
 			expectedValueValue := expectedValue.MapIndex(keyValue)
 			actualValueValue := actualValue.MapIndex(convertedKeyValue)
-			thisPrefix := append(prefix, keyValue.String())
-			if assert.True(actualValueValue.IsValid(), "missing key %s in YAML path %s", keyValue.String(), yamlPath) {
+			// keyValue.String() does *not* return the contained value, but fmt has magic for reflect.Value types
+			thisPrefix := append(prefix, fmt.Sprintf("%s", keyValue))
+			if !actualValueValue.IsValid() {
+				if expectedValueValue.Elem().Type().Kind() == reflect.Bool && expectedValueValue.Elem().Bool() == false {
+					continue
+				}
+				if expectedValueValue.Elem().Type().Kind() == reflect.Int && expectedValueValue.Elem().Int() == 0 {
+					continue
+				}
+				if expectedValueValue.Elem().Type().Kind() == reflect.String && expectedValueValue.Elem().String() == "" {
+					continue
+				}
+			}
+			if assert.True(actualValueValue.IsValid(), "missing key %s in YAML path %s", keyValue, yamlPath) {
 				if !isYAMLSubsetInner(assert, expectedValueValue.Interface(), actualValueValue.Interface(), thisPrefix) {
 					success = false
 				}
