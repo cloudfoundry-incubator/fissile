2caa160a18382e4f94686cdc07833d6299d5e097
Andreas Kupries <andreas.kupries@suse.com>
2018-03-01 15:57:23 -0800

    UEvqSutH - Updated tests Existing test `TestPodPreFlight` modified to show that it generates a nil resource. Test `TestPodMemory` added (based on TPPF) demonstrating resource memory requests and limits.

--- kube/pod_test.go
+++ kube/pod_test.go
@@ -896,10 +896,61 @@ func TestPodPostFlight(t *testing.T) {
 			containers:
 			-
 				name: post-role
+				resources: ~
 			restartPolicy: OnFailure
 	`, "\t", "    ", -1)
 	if !assert.NoError(yaml.Unmarshal([]byte(expectedYAML), &expected)) {
 		return
 	}
+	testhelpers.IsYAMLSubset(assert, expected, actual)
+}
+
+func TestPodMemory(t *testing.T) {
+	assert := assert.New(t)
+	role := podTestLoadRole(assert, "pre-role")
+	if role == nil {
+		return
+	}
+	pod, err := NewPod(role, ExportSettings{
+		Opinions:        model.NewEmptyOpinions(),
+		UseMemoryLimits: true,
+		MemLimitFactor:  3,
+	}, nil)
+	if !assert.NoError(err, "Failed to create pod from role pre-role") {
+		return
+	}
+	assert.NotNil(pod)
+
+	yamlConfig := &bytes.Buffer{}
+	if err := helm.NewEncoder(yamlConfig).Encode(pod); !assert.NoError(err) {
+		return
+	}
+
+	var expected, actual interface{}
+	if !assert.NoError(yaml.Unmarshal(yamlConfig.Bytes(), &actual)) {
+		return
+	}
+
+	expectedYAML := strings.Replace(`---
+		apiVersion: v1
+		kind: Pod
+		metadata:
+			name: pre-role
+		spec:
+			containers:
+			-
+				name: pre-role
+				resources:
+					requests:
+						memory: 128Mi
+					limits:
+						memory: 384Mi
+			restartPolicy: OnFailure
+			terminationGracePeriodSeconds: 600
+	`, "\t", "    ", -1)
+	if !assert.NoError(yaml.Unmarshal([]byte(expectedYAML), &expected)) {
+		return
+	}
+
 	testhelpers.IsYAMLSubset(assert, expected, actual)
 }
