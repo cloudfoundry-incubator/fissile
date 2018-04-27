b49a5fdca72c803a7f127c1d20da926e6c3b7f76
Andreas Kupries <andreas.kupries@suse.com>
2018-04-27 09:29:20 -0700

    Fix the base64 handling in the registry credentials tests. Also, document what we are testing when neither of host/user/pass are specified.

--- kube/registry_credentials_test.go
+++ kube/registry_credentials_test.go
@@ -1,6 +1,7 @@
 package kube
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,31 +46,42 @@ func TestMakeRegistryCredentialsForHelmWithDefaults(t *testing.T) {
 		return
 	}
 
-	// .Values.kube.registry.hostname
-	// .Values.kube.registry.username
-	// .Values.kube.registry.password
-	// json, base64 encoded
-
-	// Notes: The base64 decodes to
+	// Below we see how helm renders the registry secret when no
+	// registry (host) is specified, with no user/pass either.
+	//
+	// dcfg :=
 	// 	{%!q(<nil>):{"username":"","password":"","auth":"JSFzKDxuaWw+KTolIXMoPG5pbD4p"}}
-	// and the auth value decodes to
+	// auth :=
 	// 	%!s(<nil>):%!s(<nil>)
+
+	user := "" // default from nil
+	pass := "" // ditto
+	qnil := "%!q(<nil>)"
+	snil := "%!s(<nil>)"
+
+	auth64 := testhelpers.RenderEncodeBase64(fmt.Sprintf("%s:%s", snil, snil))
+	// user, pass are nil, and helm renders that as snil.
+
+	dcfg := testhelpers.RenderEncodeBase64(fmt.Sprintf(
+		`{%s:{"username":%q,"password":%q,"auth":%q}}`,
+		qnil, user, pass, auth64))
+	// host is nil, and rendered as qnil by helm
 
 	actual, err := testhelpers.RoundtripNode(registryCredentials, nil)
 	if !assert.NoError(err) {
 		return
 	}
-	testhelpers.IsYAMLEqualString(assert, `---
+	testhelpers.IsYAMLEqualString(assert, fmt.Sprintf(`---
 		apiVersion: "v1"
 		data:
-			.dockercfg: eyUhcSg8bmlsPik6eyJ1c2VybmFtZSI6IiIsInBhc3N3b3JkIjoiIiwiYXV0aCI6IkpTRnpLRHh1YVd3K0tUb2xJWE1vUEc1cGJENHAifX0=
+			.dockercfg: %s
 		kind: "Secret"
 		metadata:
 			name: "registry-credentials"
 			labels:
 				skiff-role-name: "registry-credentials"
 		type: "kubernetes.io/dockercfg"
-	`, actual)
+	`, dcfg), actual)
 }
 
 func TestMakeRegistryCredentialsForHelmWithUserChoice(t *testing.T) {
@@ -83,30 +95,35 @@ func TestMakeRegistryCredentialsForHelmWithUserChoice(t *testing.T) {
 		return
 	}
 
-	config := map[string]interface{}{
-		"Values.kube.registry.hostname": "the-host",
-		"Values.kube.registry.username": "the-user",
-		"Values.kube.registry.password": "the-password",
-	}
+	// And a registry with user and pass.
 
-	// Notes: The base64 decodes to
-	//	{"the-host":{"username":"the-user","password":"the-password","auth":"dGhlLXVzZXI6dGhlLXBhc3N3b3Jk"}}
-	// and the auth value decodes to
-	//	the-user:the-password
+	user := "the-user"
+	pass := "the-password"
+	host := "the-host"
+	auth64 := testhelpers.RenderEncodeBase64(fmt.Sprintf("%s:%s", user, pass))
+	dcfg := testhelpers.RenderEncodeBase64(fmt.Sprintf(
+		`{%q:{"username":%q,"password":%q,"auth":%q}}`,
+		host, user, pass, auth64))
+
+	config := map[string]interface{}{
+		"Values.kube.registry.hostname": host,
+		"Values.kube.registry.username": user,
+		"Values.kube.registry.password": pass,
+	}
 
 	actual, err := testhelpers.RoundtripNode(registryCredentials, config)
 	if !assert.NoError(err) {
 		return
 	}
-	testhelpers.IsYAMLEqualString(assert, `---
+	testhelpers.IsYAMLEqualString(assert, fmt.Sprintf(`---
 		apiVersion: "v1"
 		data:
-			.dockercfg: eyJ0aGUtaG9zdCI6eyJ1c2VybmFtZSI6InRoZS11c2VyIiwicGFzc3dvcmQiOiJ0aGUtcGFzc3dvcmQiLCJhdXRoIjoiZEdobExYVnpaWEk2ZEdobExYQmhjM04zYjNKayJ9fQ==
+			.dockercfg: %s
 		kind: "Secret"
 		metadata:
 			name: "registry-credentials"
 			labels:
 				skiff-role-name: "registry-credentials"
 		type: "kubernetes.io/dockercfg"
-	`, actual)
+	`, dcfg), actual)
 }
