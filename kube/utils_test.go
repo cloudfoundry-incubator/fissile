9a1fa3c02dcd25b16ce7b0abddb594bba124046c
Andreas Kupries <andreas.kupries@suse.com>
2018-05-01 09:32:08 -0700

    Redone the testing of kube version check, going for outcome instead of exact implementation of the condition.

--- kube/utils_test.go
+++ kube/utils_test.go
@@ -1,7 +1,10 @@
 package kube
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/SUSE/fissile/helm"
 
 	"github.com/stretchr/testify/assert"
 
@@ -95,7 +98,38 @@ func TestMakeVarName(t *testing.T) {
 func TestMinKubeVersion(t *testing.T) {
 	assert := assert.New(t)
 
-	expected := `or (gt (int .Capabilities.KubeVersion.Major) 3) (and (eq (int .Capabilities.KubeVersion.Major) 3) (ge (.Capabilities.KubeVersion.Minor | trimSuffix "+" | int) 1))`
+	// The template condition we wish to check: 3.1 <= version
+	vCheck := minKubeVersion(3, 1)
 
-	assert.Equal(expected, minKubeVersion(3, 1))
+	// Wrap it into a proper node we can render. Depending on the
+	// outcome of the version check one of the two list children
+	// below will be visible. Which of them is the outcome we are
+	// testing for.
+	vMatched := helm.Block(fmt.Sprintf("if (%s)", vCheck))
+	vMisMatch := helm.Block(fmt.Sprintf("if not (%s)", vCheck))
+	v := helm.NewList()
+	v.Add(helm.NewNode("match", vMatched))
+	v.Add(helm.NewNode("no-match", vMisMatch))
+
+	for _, testcase := range []struct {
+		Major  string
+		Minor  string
+		Result string
+	}{
+		{"2", "0", "- no-match"},
+		{"3", "0", "- no-match"},
+		{"3", "1", "- match"},
+		{"3", "2", "- match"},
+		{"4", "0", "- match"},
+	} {
+		config := map[string]interface{}{
+			"Capabilities.KubeVersion.Major": testcase.Major,
+			"Capabilities.KubeVersion.Minor": testcase.Minor,
+		}
+		actual, err := testhelpers.RoundtripNode(v, config)
+		if !assert.NoError(err) {
+			return
+		}
+		testhelpers.IsYAMLEqualString(assert, testcase.Result, actual)
+	}
 }
